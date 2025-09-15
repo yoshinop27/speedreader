4240610e4a4e69c1d9fde10a2f291800fe34cba4
yoshinop27 <yoshinop@grinnell.edu>
2025-09-14 19:29:44 -0500

    integrated cml args

--- src/main/java/edu/grinnell/csc207/speedreader/SpeedReader.java
+++ src/main/java/edu/grinnell/csc207/speedreader/SpeedReader.java
@@ -8,27 +8,48 @@ import java.awt.*;
 import java.lang.Integer;
 
 public class SpeedReader {
+    // setting up our global variables
+    public static int width;
+    public static int height;
+    public static int fontSize;
+    public static int wpm;
+
     // Main class will take filename, width, height, font-size, wpm
-    public static void main(String[] args) throws IOException, InterruptedException {
-        DrawingPanel panel = new DrawingPanel(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+    static void main(String[] args) throws IOException, InterruptedException, IllegalArgumentException {
+
+        // error checking
+        if (args.length != 5){
+            throw new IllegalArgumentException("Five arguments are required.");
+        }
+
+        // initialize are vars using cml args
+        width = Integer.parseInt(args[1]);
+        height = Integer.parseInt(args[2]);
+        fontSize = Integer.parseInt(args[3]);
+        wpm = Integer.parseInt(args[4]);
+
+        // create drawing panel and graphics
+        DrawingPanel panel = new DrawingPanel(width, height);
         Graphics g = panel.getGraphics();
-        Font f = new Font("Courier", Font.BOLD, 46);
+        Font f = new Font("Courier", Font.BOLD, fontSize);
         g.setFont(f);
 
+        // create generator
         WordGenerator generator = new WordGenerator("corpus/" + args[0]);
 
+        // read words and draw
         while (generator.hasNext()) {
             String word = generator.next();
 
             // clear panel
             g.setColor(Color.WHITE);
-            g.fillRect(0, 0, 400, 300);
+            g.fillRect(0, 0, width, height);
 
             // print word
             g.setColor(Color.BLACK);
-            g.drawString(word, 100, 100);
+            g.drawString(word, width/2, height/2);
 
-            Thread.sleep(1000);
+            Thread.sleep(wpm);
         }
     }
 }
